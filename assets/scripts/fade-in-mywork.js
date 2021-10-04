2a333c89201c42a607cb5c26ccbb8a3413a87a1a
Liam O'Connor <liamoconnor87@gmail.com>
2021-10-04 11:52:09 +0100

    JS: Scroll animation added - AOS framework used for fade-ins.

--- assets/scripts/fade-in-mywork.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-	// FADE IN OF 'NEED A WEBSITE' ON LANDING PAGE
-	$('.mywork-container').delay(200).animate({
-		opacity: 1
-	},800);
-
-})
--- assets/scripts/fade-in-welcome.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-	// FADE IN OF 'NEED A WEBSITE' ON LANDING PAGE
-	$('.wel-container').animate({
-		opacity: 1
-	},500);
-
-})
