249f6bb637da0a000da5dc1710c7076179dacb91
Liam O'Connor <liamoconnor87@gmail.com>
2021-10-04 13:22:01 +0100

    Maint: JS syntax amended.

--- assets/scripts/mouse_hover.js
+++ assets/scripts/mouse_hover.js
@@ -4,26 +4,26 @@ $(document).ready(function(){
     $('.navbar').mouseenter(function(){
         $(this).removeClass('navbar-bg');
         $(this).addClass('navbar-bg-solid navbar-shad');
-    })
+    });
 
     // NAVBAR
     $('.navbar').mouseleave(function(){
         $(this).removeClass('navbar-bg-solid navbar-shad');
         $(this).addClass('navbar-bg');
-    })
+    });
 
     // MY WORK
     $('.preview-overlay').mouseenter(function(){
         $(this).animate({
             opacity: 0
         },200);
-    })
+    });
 
     // MY WORK
     $('.preview-overlay').mouseleave(function(){
         $(this).animate({
             opacity: 1
         },200);
-    })
+    });
 
 });
