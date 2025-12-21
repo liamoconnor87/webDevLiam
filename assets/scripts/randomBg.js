39f8b723c80f8d95a162d482ca1ee92c7a40e54d
Liam O'Connor <liamoconnor87@gmail.com>
2025-12-21 19:42:39 +0000

    refactor shuffle

--- assets/scripts/randomBg.js
+++ assets/scripts/randomBg.js
@@ -1,188 +1,76 @@
+
+// --- Refactored randomBg.js ---
+// Maintainer notes:
+// - Consider migrating to vanilla JS for future-proofing and performance.
+// - This refactor uses a config mapping and a generic assignment function.
+// - Chrome and Firefox links always match the color of text-three and text-five, respectively.
+
 function shuffleColours() {
-	// Need to add each one of 'text-one' , 'text-two' & 'text-three' ...
-	// ... as a class so that it can add the text colour class.
-	// Refer to JS_animations.css for what colours are added.
-
-	// Shuffles the Array bgColoursArray and returns its shuffled order.
-	function shufflesBgColours() {
-		let bgColoursArray  = ["cyan", "lilac", "yellow", "green", "red"];
-		// Ref - https://www.w3docs.com/snippets/javascript/how-to-randomize-shuffle-a-javascript-array.html
-		let curId = bgColoursArray.length;
-		while (0 !== curId) {
-			let randId = Math.floor(Math.random() * curId);
-			curId -=1;
-
-			let tmp = bgColoursArray[curId];
-			bgColoursArray[curId] = bgColoursArray[randId];
-			bgColoursArray[randId] = tmp;
+	// Color and class mapping for each project type
+	const colorOrder = ["cyan", "lilac", "yellow", "green", "red"];
+	const textClasses = {
+		cyan: "text-cyan",
+		lilac: "text-lilac",
+		yellow: "text-yel",
+		green: "text-green",
+		red: "text-red"
+	};
+	const iconClasses = {
+		one: ["icon-4", "icon", "icon-2", "icon-3", "icon-4"], // nav-icon-3
+		two: ["icon", "icon-4", "icon-3", "icon-2", "icon-2"],   // nav-icon-2
+		three: ["icon", "icon-4", "icon-3", "icon-2", "icon-2"], // nav-icon-1
+	};
+	const extLinkClasses = {
+		one: ["ext-icon", "ext-icon-4", "ext-icon-3", "ext-icon-2", "ext-icon-2"],
+		two: ["ext-icon", "ext-icon-4", "ext-icon-3", "ext-icon-2", "ext-icon-2"],
+		three: ["ext-icon", "ext-icon-4", "ext-icon-3", "ext-icon-2", "ext-icon-4"]
+	};
+	// Shuffle color order
+	function shuffleArray(arr) {
+		let a = arr.slice();
+		for (let i = a.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[a[i], a[j]] = [a[j], a[i]];
 		}
-		// Returns new Array of bgColoursArray
-		return bgColoursArray;
+		return a;
 	}
+	const shuffledColors = shuffleArray(colorOrder);
 
-	// Original order of array.
+	// Remove all color and icon classes
+	const allTextClasses = Object.values(textClasses).join(" ");
+	[1,2,3,4,5].forEach(i => {
+		$(`.text-one, .text-two, .text-three, .text-four, .text-five`).removeClass(allTextClasses);
+	});
+	$(".nav-icon-1, .nav-icon-2, .nav-icon-3").removeClass("icon icon-2 icon-3 icon-4");
+	$(".ext-link-1, .ext-link-2, .ext-link-3").removeClass("ext-icon ext-icon-2 ext-icon-3 ext-icon-4");
+	$(".trr-title-special").removeClass("trr-underline-cyan trr-underline-yellow trr-underline-lilac trr-underline-green trr-underline-red");
 
-	bgColoursArray = shufflesBgColours();
-
-	// This will display new order of Array.
-	let randBgOneArray =  bgColoursArray[0];
-	let randBgTwoArray = bgColoursArray[1];
-	let randBgThreeArray = bgColoursArray[2];
-	let randBgFourArray = bgColoursArray[3];
-	let randBgFiveArray = bgColoursArray[4];
-
-	// Converts each new Arrays into Sting.
-	let randBgOne = randBgOneArray.toString();
-	let randBgTwo = randBgTwoArray.toString();
-	let randBgThree = randBgThreeArray.toString();
-	let randBgFour = randBgFourArray.toString();
-	let randBgFive = randBgFiveArray.toString();
-
-
-	// Functions that determine which array sets which colour of text.
-	function backgroundOne() {
-		// Remove previous underline color classes from special title
-		var $specialTitle = $(".trr-title-special");
-		$specialTitle.removeClass("trr-underline-cyan trr-underline-yellow trr-underline-lilac trr-underline-green trr-underline-red");
-		if (randBgOne == "cyan") {
-			$(".text-one").addClass("text-cyan");
-			$(".nav-icon-3").addClass("icon-4");
-			$specialTitle.addClass("trr-underline-cyan");
-		} else if (randBgOne == "yellow") {
-			$(".text-one").addClass("text-yel");
-			$(".nav-icon-3").addClass("icon");
-			$specialTitle.addClass("trr-underline-yellow");
-		} else if (randBgOne == "lilac") {
-			$(".text-one").addClass("text-lilac");
-			$(".nav-icon-3").addClass("icon-2");
-			$specialTitle.addClass("trr-underline-lilac");
-		} else if (randBgOne == "green") {
-			$(".text-one").addClass("text-green");
-			$(".nav-icon-3").addClass("icon-3");
-			$specialTitle.addClass("trr-underline-green");
-		} else if (randBgOne == "red") {
-			$(".text-one").addClass("text-red");
-			$(".nav-icon-3").addClass("icon-4");
-			$specialTitle.addClass("trr-underline-red");
-		} else {
-			console.log("Error with BG One");
+	// Assign color classes
+	function assignColor(idx, textSel, navSel, extSel, extSyncClass) {
+		const color = shuffledColors[idx];
+		const textClass = textClasses[color];
+		$(textSel).addClass(textClass);
+		if (navSel) $(navSel).addClass(iconClasses[navSel]?.[idx] || "");
+		if (extSel) $(extSel).addClass(extLinkClasses[extSel]?.[idx] || "");
+		// Special underline for trr
+		if (textSel === ".text-one") {
+			const underlineClass = `trr-underline-${color}`;
+			$(".trr-title-special").addClass(underlineClass);
 		}
-
-	}
-
-	function backgroundTwo() {
-		if (randBgTwo == "cyan") {
-			$(".text-two").addClass("text-cyan");
-			$(".nav-icon-2").addClass("icon");
-			$(".ext-link-2").addClass("ext-icon");
-		} else if (randBgTwo == "yellow") {
-			$(".text-two").addClass("text-yel");
-			$(".nav-icon-2").addClass("icon-3");
-			$(".ext-link-2").addClass("ext-icon-3");
-		} else if (randBgTwo == "lilac") {
-			$(".text-two").addClass("text-lilac");
-			$(".nav-icon-2").addClass("icon-4");
-			$(".ext-link-2").addClass("ext-icon-4");
-		} else if (randBgTwo == "green") {
-			$(".text-two").addClass("text-green");
-			$(".nav-icon-2").addClass("icon-2");
-			$(".ext-link-2").addClass("ext-icon-2");
-		}else if (randBgTwo == "red") {
-			$(".text-two").addClass("text-red");
-			$(".nav-icon-2").addClass("icon-2");
-			$(".ext-link-2").addClass("ext-icon-2");
-		} else {
-			console.log("Error with BG Two");
+		// Chrome link matches text-three
+		if (extSyncClass === "chrome") {
+			$(".ext-link-1").removeClass(allTextClasses).addClass(textClass);
 		}
-
-	}
-
-	function backgroundThree() {
-		if (randBgThree == "cyan") {
-			$(".text-three").addClass("text-cyan");
-			$(".nav-icon-1").addClass("icon");
-			$(".ext-link-1").addClass("ext-icon");
-            $(".ext-link-1").removeClass("text-yel text-lilac text-green text-red").addClass("text-cyan");
-		} else if (randBgThree == "yellow") {
-			$(".text-three").addClass("text-yel");
-			$(".nav-icon-1").addClass("icon-3");
-			$(".ext-link-1").addClass("ext-icon-3");
-            $(".ext-link-1").removeClass("text-cyan text-lilac text-green text-red").addClass("text-yel");
-		} else if (randBgThree == "lilac") {
-			$(".text-three").addClass("text-lilac");
-			$(".nav-icon-1").addClass("icon-4");
-			$(".ext-link-1").addClass("ext-icon-4");
-            $(".ext-link-1").removeClass("text-cyan text-yel text-green text-red").addClass("text-lilac");
-		} else if (randBgThree == "green") {
-			$(".text-three").addClass("text-green");
-			$(".nav-icon-1").addClass("icon-2");
-			$(".ext-link-1").addClass("ext-icon-2");
-            $(".ext-link-1").removeClass("text-cyan text-yel text-lilac text-red").addClass("text-green");
-		}  else if (randBgThree == "red") {
-			$(".text-three").addClass("text-red");
-			$(".nav-icon-1").addClass("icon-2");
-			$(".ext-link-1").addClass("ext-icon-2");
-            $(".ext-link-1").removeClass("text-cyan text-yel text-lilac text-green").addClass("text-red");
-		} else {
-			console.log("Error with BG Three");
+		// Firefox link matches text-five
+		if (extSyncClass === "firefox") {
+			$(".ext-link-2").removeClass(allTextClasses).addClass(textClass);
 		}
-
 	}
-
-	function backgroundFour() {
-
-		if (randBgFour == "cyan") {
-			$(".text-four").addClass("text-cyan");
-		} else if (randBgFour == "yellow") {
-			$(".text-four").addClass("text-yel");
-		} else if (randBgFour == "lilac") {
-			$(".text-four").addClass("text-lilac");
-		} else if (randBgFour == "green") {
-			$(".text-four").addClass("text-green");
-		} else if (randBgFour == "red") {
-			$(".text-four").addClass("text-red");
-		} else {
-			console.log("Error with BG Four");
-		}
-
-	}
-
-	function backgroundFive() {
-
-		if (randBgFive == "cyan") {
-			$(".text-five").addClass("text-cyan");
-			$(".ext-link-3").addClass("ext-icon");
-            $(".ext-link-2").removeClass("text-yel text-lilac text-green text-red").addClass("text-cyan");
-		} else if (randBgFive == "yellow") {
-			$(".text-five").addClass("text-yel");
-			$(".ext-link-3").addClass("ext-icon-3");
-            $(".ext-link-2").removeClass("text-cyan text-lilac text-green text-red").addClass("text-yel");
-		} else if (randBgFive == "lilac") {
-			$(".text-five").addClass("text-lilac");
-			$(".ext-link-3").addClass("ext-icon-4");
-            $(".ext-link-2").removeClass("text-cyan text-yel text-green text-red").addClass("text-lilac");
-		} else if (randBgFive == "green") {
-			$(".text-five").addClass("text-green");
-			$(".ext-link-3").addClass("ext-icon-2");
-            $(".ext-link-2").removeClass("text-cyan text-yel text-lilac text-red").addClass("text-green");
-		} else if (randBgFive == "red") {
-			$(".text-five").addClass("text-red");
-			$(".ext-link-3").addClass("ext-icon-4");
-            $(".ext-link-2").removeClass("text-cyan text-yel text-lilac text-green").addClass("text-red");
-		} else {
-			console.log("Error with BG Five");
-		}
-
-	}
-
-	function bgGenerator(){
-		backgroundOne();
-		backgroundTwo();
-		backgroundThree();
-		backgroundFour();
-		backgroundFive();
-	}
-	bgGenerator();
+	assignColor(0, ".text-one", ".nav-icon-3", null);
+	assignColor(1, ".text-two", ".nav-icon-2", ".ext-link-2");
+	assignColor(2, ".text-three", ".nav-icon-1", ".ext-link-1", "chrome");
+	assignColor(3, ".text-four");
+	assignColor(4, ".text-five", null, ".ext-link-3", "firefox");
 }
 
 function reshuffleBgColours() {
